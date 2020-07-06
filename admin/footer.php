c1a40d7cc8cd3850390c5d65542d57ad0c0178c8
Anonymous26s <Anonymous_26s@hotmail.com>
2020-07-05 18:50:44 -0700

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Desarrollado por <a href="http://obedalvarado.pw/" target='_blank'>Sonora Online Security</a></p>
+      </div>
+</footer>
